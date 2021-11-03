957c7eade4e8f4361e00527e2199fe72798eaf79
KImSuim <suim7171@naver.com>
2021-11-03 17:46:44 +0900

    .

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //넷바 색상 변경
 // let :변하는 상수 | const :변하지 않는 상수
-const navbar =document.puerySelector('#navbar')
+const navbar =document.querySelector('#navbar')
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
     if(window. scrollY > navbarHeight){
@@ -12,3 +12,25 @@ document.addEventListener("scroll", () => {
     }
 
 });
+
+//넷바메뉴 선택시 해당 부분으로 이동하기
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener("click", (event)=>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    scrollIntoView(link);
+});
+
+//contact me 버튼 이동하기
+const contactMeBtn = document.querySelector(".home__contact");
+contactMeBtn.addEventListener("click", ()=> {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior : 'smooth'});
+}
