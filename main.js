2de97c62f3e89554343ba11ea6b6d03d4ea7b753
KImSuim <suim7171@naver.com>
2021-11-10 17:50:44 +0900

    z

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //넷바 색상 변경
 // let :변하는 상수 | const :변하지 않는 상수
-const navbar =document.querySelector('#navbar')
+const navbar = document.querySelector('#navbar')
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
     if(window. scrollY > navbarHeight){
